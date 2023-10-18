5a294f71e404116d94683431e94b190b248a4d1e
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-10-18 17:45:38 -0400

    hi

---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [Header("Spawn Cube Object")]
+    public GameObject spawnCube;
+    [Header("Default Difficulty")]
+    public float difficulty = 40f;
+
+    float spawn;
+    // Update is called once per frame
+    void Update()
+    {
+        spawn = difficulty * Time.deltaTime;
+        difficulty = Time.deltaTime * 4f;
+        while (spawn > 0)
+        {
+            spawn -= 1;
+            //Vector3 v3Pos
+        }
+    }
+}
